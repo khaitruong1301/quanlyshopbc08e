4adbab0dba79d5c2555ef6f12f4230b921d373ae
macbook <khaitruong1301@gmail.com>
2021-06-20 11:23:15 +0700

    devb_tao_nhanh

--- controllers/devB.js
+++ controllers/devB.js
@@ -3,3 +3,6 @@ console.log('Dev b code chức năng này nha !');
 function devB() {
     console.log('Ahi hi sĩ đẹp trai quá !!!!');
 }
+function createBranchDevB() {
+    console.log('Vui quá mình vừa mới tạo nhánh devB nè!!!');
+}
